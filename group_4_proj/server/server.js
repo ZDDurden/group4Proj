922b3fae97258f5f0e748a1e2cdb82ac36e6eb31
student <student@Admins-iMac.local>
2019-12-02 12:48:51 -0600

    rw

--- group_4_proj/server/server.js
+++ group_4_proj/server/server.js
@@ -1,47 +1,64 @@
 const express = require("express");
-const bodyParser = require("body-parser");
 const app = express();
 require("dotenv").config();
-// const MongoClient = require("mongodb").MongoClient;
-// const url = `mongodb+srv://zddurden:${process.env.DB_PASS}@cluster0-hamuy.mongodb.net/test?retryWrites=true&w=majority`;
-// const client = new MongoClient(url, { useNewUrlParser: true, useUnifiedTopology: true });
-// const assert = require("assert");
 const {
   Stitch,
   RemoteMongoClient,
   AnonymousCredential
-} = require('mongodb-stitch-server-sdk');
+} = require("mongodb-stitch-server-sdk");
 // const {
 //   Stitch,
 //   RemoteMongoClient,
 //   AnonymousCredential
 // } = require('mongodb-stitch-react-native-sdk');
+const client = Stitch.initializeDefaultAppClient("group4proj-kjjhf");
 
-const client = Stitch.initializeDefaultAppClient('group4proj-kjjhf');
+const db = client
+  .getServiceClient(RemoteMongoClient.factory, "mongodb-atlas")
+  .db("Group4Project");
 
-const db = client.getServiceClient(RemoteMongoClient.factory, 'mongodb-atlas').db('Group4Project');
-
-client.auth.loginWithCredential(new AnonymousCredential()).then(() =>
-  db.collection('Group4').find({}, { limit: 100}).asArray()
-).then(docs => {
-    console.log("Found docs", docs)
-    console.log("[MongoDB Stitch] Connected to Stitch")
-}).catch(err => {
-    console.error(err)
-});
-
-// client.connect(function(err, db) {
-//   if (err) throw err;
-//   var dbo = db.db("Group4Proj");
-//   dbo.collection("Group4").find({}).toArray(function(err, result) {
-//     if (err) throw err;
-//     console.log(result);
-//     db.close();
-//   });
-// });
+client.auth
+  .loginWithCredential(new AnonymousCredential())
+  // .then(user =>
+  //   db
+  //     .collection("events")
+  //     .insertOne(
+  //       { band: "Motorhead", location: "London", date: Date() }
+  //     )
+  .then(() =>
+    db
+      .collection("Group4")
+      .find({}, { limit: 100 })
+      .asArray()
+  )
+  .then(docs => {
+    console.log("Found docs", docs);
+  })
+  .then(() =>
+    db
+      .collection("events")
+      .find({}, { limit: 100 })
+      .asArray()
+  )
+  .then(docs => {
+    console.log("Found docs", docs);
+  })
+  .then(() =>
+    db
+      .collection("users")
+      .find({}, { limit: 100 })
+      .asArray()
+  )
+  .then(docs => {
+    console.log("Found docs", docs);
+    console.log("[MongoDB Stitch] Connected to Stitch");
+  })
+  .catch(err => {
+    console.error(err);
+  });
 
 app.get("/", (req, res) => {
-  res.send("YOUR EXPRESS BACKEND IS CONNECTED TO REACT");
+  res.send("MongoDB is connected");
   console.log("YOUR EXPRESS BACKEND IS CONNECTED TO REACT");
 });
 
