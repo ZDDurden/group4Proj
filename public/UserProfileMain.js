189da77604a35378c0c6a5f3ae9d8c0863597af8
Ckelsie Sukhai <54908203+ckyalaterr@users.noreply.github.com>
2019-12-03 17:21:17 -0600

    started user profile

---
+++ public/UserProfileMain.js
@@ -0,0 +1,14 @@
+import React from "react";
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar
+} from "react-native";
+import WebView from "react-native-webview";
+
+export default class UserProfile extends React.Component{
+
+}
