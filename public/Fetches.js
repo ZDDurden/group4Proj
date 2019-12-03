aa0a2ce0cd4f36e27269d73c6a4bea0d300af821
student <student@Admins-iMac.local>
2019-12-03 15:52:45 -0600

    updated imports

--- public/Fetches.js
+++ public/Fetches.js
@@ -1,4 +1,5 @@
-import React, { Component, Text, View } from 'react';
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
 
 export default class Fetches extends Component {
     render() {
