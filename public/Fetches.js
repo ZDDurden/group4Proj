379e19852b3e324a8f1c4302cf89184d263002ab
student <student@Admins-iMac.local>
2019-12-03 15:23:23 -0600

    fetch

--- public/Fetches.js
+++ public/Fetches.js
@@ -1,7 +1,6 @@
 import React, { Component, Text, View } from 'react'
 
 export default class Fetches extends Component {
-
     render() {
         try {
             let [bands, users, events] = await Promise.all([
