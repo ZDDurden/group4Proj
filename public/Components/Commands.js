09dd6e70d02d6535c5e12583374e51cf0e83b649
student <student@Admins-iMac.local>
2019-12-04 11:24:20 -0600

    added commands file

---
+++ public/Components/Commands.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+
+export default class Commands extends Component {
+    async getInfo(){
+        
+    }
+    async postInfo(){
+
+    }
+    async putInfo(){
+
+    }
+    async deleteInfo(){
+
+    }
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
