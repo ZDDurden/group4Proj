c5bc43c470fa2beedd0f5cb763b82b162da34607
student <student@Admins-iMac.local>
2019-12-05 09:21:21 -0600

    edited put and delete routing

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -19,7 +19,6 @@ const db = client
 app.use(express.json());
 
 module.exports = function(app) {
-  
   //Get routes
 
   app.get("/", (req, res) => {
@@ -203,17 +202,21 @@ module.exports = function(app) {
       spotify: req.body.spotify,
       social: req.body.social
     };
-    let query = { _id: req.params.id },
+    let query = { _id: ObjectID(req.params.id) },
       body = { $set: data },
       opts = {
         returnOriginal: false,
         upsert: true
       };
-    client.auth.loginWithCredential(new AnonymousCredential());
-    db.collection("Group4")
-      .findOneAndUpdate(query, body, opts)
-      .then(doc => res.send(204))
-      .catch(err => res.send(500, err));
+    client.auth
+      .loginWithCredential(new AnonymousCredential())
+      .then(() => {
+        db.collection("Group4")
+          .findOneAndUpdate(query, body, opts)
+          .then(doc => res.send(204))
+          .catch(err => res.send(500, err));
+      })
+      .catch(err => console.log(err));
   });
   app.put("/users/:id", (req, res) => {
     const data = {
@@ -227,17 +230,21 @@ module.exports = function(app) {
       bands: req.body.bands,
       genres: req.body.genres
     };
-    let query = { _id: req.params.id },
+    let query = { _id: ObjectID(req.params.id) },
       body = { $set: data },
       opts = {
         returnOriginal: false,
         upsert: true
       };
-    client.auth.loginWithCredential(new AnonymousCredential());
-    db.collection("users")
-      .findOneAndUpdate(query, body, opts)
-      .then(doc => res.send(204))
-      .catch(err => res.send(500, err));
+    client.auth
+      .loginWithCredential(new AnonymousCredential())
+      .then(() => {
+        db.collection("users")
+          .findOneAndUpdate(query, body, opts)
+          .then(doc => res.send(204))
+          .catch(err => res.send(500, err));
+      })
+      .catch(err => console.log(err));
   });
   app.put("/events/:id", (req, res) => {
     const data = {
@@ -245,40 +252,56 @@ module.exports = function(app) {
       location: req.body.location,
       date: req.body.date
     };
-    let query = { _id: req.params.id },
+    let query = { _id: ObjectID(req.params.id) },
       body = { $set: data },
       opts = {
         returnOriginal: false,
         upsert: true
       };
-    client.auth.loginWithCredential(new AnonymousCredential());
-    db.collection("events")
-      .findOneAndUpdate(query, body, opts)
-      .then(doc => res.send(204))
-      .catch(err => res.send(500, err));
+    client.auth
+      .loginWithCredential(new AnonymousCredential())
+      .then(() => {
+        db.collection("events")
+          .findOneAndUpdate(query, body, opts)
+          .then(doc => res.send(204))
+          .catch(err => res.send(500, err));
+      })
+      .catch(err => console.log(err));
   });
 
   //Delete routes
 
   app.delete("/bands/:id", (req, res) => {
-    client.auth.loginWithCredential(new AnonymousCredential());
-    db.collection("Group4")
-      .findOneAndDelete({ _id: req.params.id })
-      .then(doc => res.send(204))
-      .catch(err => res.send(500, err));
+    client.auth
+      .loginWithCredential(new AnonymousCredential())
+      .then(() => {
+        db.collection("Group4")
+          .findOneAndDelete({ _id: ObjectID(req.params.id) })
+          .then(doc => res.send(204))
+          .catch(err => res.send(500, err));
+      })
+      .catch(err => console.log(err));
   });
   app.delete("/users/:id", (req, res) => {
-    client.auth.loginWithCredential(new AnonymousCredential());
-    db.collection("users")
-      .findOneAndDelete({ _id: req.params.id })
-      .then(doc => res.send(204))
-      .catch(err => res.send(500, err));
+    client.auth
+      .loginWithCredential(new AnonymousCredential())
+      .then(() => {
+        db.collection("users")
+          .findOneAndDelete({ _id: ObjectID(req.params.id) })
+          .then(doc => res.send(204))
+          .catch(err => res.send(500, err));
+      })
+      .catch(err => console.log(err));
   });
   app.delete("/events/:id", (req, res) => {
-    client.auth.loginWithCredential(new AnonymousCredential());
-    db.collection("events")
-      .findOneAndDelete({ _id: req.params.id })
-      .then(doc => res.send(204))
-      .catch(err => res.send(500, err));
+    client.auth
+      .loginWithCredential(new AnonymousCredential())
+      .then(() => {
+        db.collection("events")
+          .findOneAndDelete({ _id: ObjectID(req.params.id) })
+          .then(doc => res.send(204))
+          .catch(err => res.send(500, err));
+      })
+      .catch(err => console.log(err));
   });
 };
