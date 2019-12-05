1af933ce6b02c8ef5fa2e15eadc2eaf78ade4282
student <student@Admins-iMac.local>
2019-12-05 14:08:15 -0600

    working on fetches on userprofile

--- public/Components/userView/UserProfile.js
+++ public/Components/userView/UserProfile.js
@@ -1,79 +1,99 @@
 import React from "react";
 import {
-	SafeAreaView,
-	StyleSheet,
-	ScrollView,
-	View,
-	Text,
-	StatusBar,
-	TouchableOpacity,
-	Image
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+  TouchableOpacity,
+  Image
 } from "react-native";
 import { NativeRouter, Route, Link } from "react-router-native";
-import Header from '../Header'
-import Fetches from '../Fetches'
-import styles from '../Styles'
+import Header from "../Header";
+import Fetches from "../Fetches";
+import styles from "../Styles";
 
 class UserProfile extends React.Component {
-	constructor() {
-		super();
+  constructor(props) {
+    super(props);
 
-		this.setState = {
-			users: [],
-			isLoaded: true
-		};
-	}
-	componentDidMount() {
-		fetch("https://localhost:3000/users")
-			.then(response => response.json())
-			.then(result => this.setState({ users: result }));
-	}
-	handleUserPost = async () => {
-		fetch("https://localhost:3000/users", {
-			method: "POST",
-			headers: {
-				"Content-Type": "application/json"
-			},
-			body: JSON.stringify({
-				name: req.body.value,
-				email: req.body.value,
-				password: req.body.value,
-				dob: req.body.value,
-				location: req.body.value,
-				likes: req.body.value,
-				pic: req.body.value,
-				bands: req.body.value,
-				genres: req.body.value
-			})
-		})
-	};
+    this.state = {
+	  name: "",
+	  location: "",
+	  likes: "",
+      isLoaded: false
+    };
+  }
+  componentDidMount() {
+    return fetch("https://localhost:3000/users/5de587643fab7bf2c5529383", {
+      method: "GET",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json"
+      }
+    })
+      .then(response => {
+        alert(Received);
+        return response.json();
+      })
+      .then(result => {
+		  const { name, location, likes } = result
+        this.setState({
+		  name: name,
+		  location: location,
+		  likes: likes,
+          isLoaded: true
+        });
+        alert(this.state.name);
+      })
+      .catch(error => {
+        alert("request failed", error);
+      });
+  }
 
-	render() {
-		return (
-			<View style={styles.container}>
-				<View style={styles.header}></View>
-				<Image style={styles.avatar} source={{ uri: 'https://icon-library.net/images/default-user-icon/default-user-icon-11.jpg' }} />
-				<View style={styles.body}>
-					<View style={styles.bodyContent}>
-						<Text style={styles.name}>test</Text>
-						<Text style={styles.info}>Bio</Text>
+  render() {
+    if (!this.state.isLoaded) {
+      return (
+        <View>
+          <Text>Loading...</Text>
+        </View>
+      );
+    } else {
+      return (
+        <View style={styles.container}>
+          <View style={styles.header}></View>
+          <Image
+            style={styles.avatar}
+            source={{
+              uri:
+                "https://icon-library.net/images/default-user-icon/default-user-icon-11.jpg"
+            }}
+          />
+          <View style={styles.body}>
+            <View style={styles.bodyContent}>
+              <Text style={styles.name}>{this.state.name}</Text>
+              <Text style={styles.info}>Bio</Text>
 
-
-						<Link to="/Settings" underlayColor="#f0f4f7">
-							<TouchableOpacity style={styles.buttonContainer} title="Settings">
-								<Text>Settings</Text>
-							</TouchableOpacity>
-						</Link>
-						<Link to="/Edit" underlayColor="#f0f4f7">
-							<TouchableOpacity style={styles.buttonContainer} title="Edit">
-								<Text>Edit</Text>
-							</TouchableOpacity>
-						</Link>
-					</View>
-				</View>
-			</View>
-		);
-	}
+              <Link to="/Settings" underlayColor="#f0f4f7">
+                <TouchableOpacity
+                  style={styles.buttonContainer}
+                  title="Settings"
+                >
+                  <Text>Settings</Text>
+                </TouchableOpacity>
+              </Link>
+              <Link to="/Edit" underlayColor="#f0f4f7">
+                <TouchableOpacity style={styles.buttonContainer} title="Edit">
+                  <Text>Edit</Text>
+                </TouchableOpacity>
+              </Link>
+            </View>
+          </View>
+        </View>
+      );
+    }
+  }
 }
 
-export default UserProfile
+export default UserProfile;
--- server/server.js
+++ server/server.js
@@ -1,7 +1,17 @@
 const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
 const app = express();
 
-require('./routes/routes')(app)
+app.use(cors());
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: false
+  })
+);
+
+require("./routes/routes")(app);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
