08a8bfafd5ef7db616317a56210ba1220fe50ccd
Ckelsie Sukhai <54908203+ckyalaterr@users.noreply.github.com>
2019-12-09 15:25:52 -0600

    Band stats mock

--- public/Components/bandView/BandStats.js
+++ public/Components/bandView/BandStats.js
@@ -6,19 +6,80 @@ import {
   View,
   Text,
   StatusBar,
+  TouchableOpacity,
+  Image,
+  ActivityIndicator, AsyncStorage, ImageBackground
 } from "react-native";
-import styles from '../Styles'
+import styles from "../Styles";
 
+const id = '5de5848383c0259731bbe274'
 
-class BandStats extends React.Component {
+export default class BandStats extends React.Component {
+  constructor(props) {
+    super(props);
 
+    this.state = {
+      bands: [],
+      isLoaded: false
+    };
+  }
+  static navigationOptions = {
+    header: null,
+};
+  componentDidMount() {
+    // const fetchConfig = {
+    //   method: 'POST',
+    //   headers: {
+    //     'Accept': 'application/json',
+    //     'Content-Type': 'application/json'
+    //   }}
+    fetch(`https://banderapi.herokuapp.com/bands/${id}`)
+      .then(response => {
+        return response.json();
+      })
+      .then(result => {
+        this.setState({
+          bands: result,
+          isLoaded: true,
+          // id: result._id
+        })
+        console.log(this.state.bands);
+      })
+      .catch(error => {
+        alert("request failed", error);
+      });
+  }
   render() {
+    if (!this.state.isLoaded) {
+      return (
+        <View>
+          <Text>Loading...</Text>
+        </View>
+      );
+    } else {
     return (
-      <View style={styles.bandBody}>
-        <Text style={styles.bandName}>Nothing here yet!</Text>
-      </View>
-    );
+      <View style={styles.container}>
+        <ImageBackground style={styles.header} source={{
+            uri:
+              this.state.bands.banner
+          }}></ImageBackground>
+        <Image
+          style={styles.avatar}
+          source={{
+            uri:
+              this.state.bands.logo
+          }}
+        />
+          <View style={styles.body}>
+            <View style={styles.bodyContent}>
+              <Text style={styles.name}>User likes: 203</Text>
+              <Text style={styles.name}>User listens: 500</Text>
+              <Text style={styles.name}>Date joined: 12/01/19</Text>
+
+            </View>
+          </View>
+        </View>
+      );
+    }
   }
 }
-
-export default BandStats;
